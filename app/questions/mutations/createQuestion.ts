ce257d35aabbf48c0436cb3f62848e71191f2abe
Colton Kaiser <kaiserc2@seattleu.edu>
2021-06-14 12:55:52 -0600

    Add functionality for creating choices.

--- app/questions/mutations/createQuestion.ts
+++ app/questions/mutations/createQuestion.ts
@@ -3,12 +3,15 @@ import db from "db"
 import { z } from "zod"
 
 const CreateQuestion = z.object({
-  name: z.string(),
+  text: z.string(),
+  choices: z.array(z.object({ text: z.string() })),
 })
 
 export default resolver.pipe(resolver.zod(CreateQuestion), resolver.authorize(), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const question = await db.question.create({ data: input })
+  const question = await db.question.create({
+    data: { ...input, choices: { create: input.choices } },
+  })
 
   return question
 })
